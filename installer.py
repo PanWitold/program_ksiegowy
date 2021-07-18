d8b60ee212ab9ec99869039506e3e25e4bf2e790
PanWitold <witold001@gmail.com>
2021-07-18 22:36:04 +0200

    cleaning

--- installer.py
+++ installer.py
@@ -1,24 +1,26 @@
 import subprocess
 import os
 
-extended = ".py"
-path_to_main_dir = r"C:\Users\PC\Desktop\serwer\!serwer"
-path_to_output = rf"C:\Users\PC\Desktop\\"
-
-path_to_zamowienia = path_to_main_dir + r"\zamowienia" + extended
-path_to_dostawy = path_to_main_dir + r"\dostawy" + extended
-path_to_uzytkownicy = path_to_main_dir + r"\uzytkownicy" + extended
-
+extended = ".pyw"       # py z terminalem, pyw bez terminala
+path_to_main_dir = "/media/main_application"
+path_to_output = "/var/www/html/"
+# absolute path for all of files
+path_to_zamowienia = path_to_main_dir + "/zamowienia" + extended
+path_to_dostawy = path_to_main_dir + "/dostawy" + extended
+path_to_uzytkownicy = path_to_main_dir + "/uzytkownicy" + extended
+# list with path files
 files = [path_to_zamowienia, path_to_dostawy, path_to_uzytkownicy]
 
 
 def execute_installer(path_file):
-    if del_exists(path_file):
-        print(f"Nazwa pliku: {path_file}")
-        name = f"python -m pyinstaller --onefile --distpath {path_to_output} {path_file}"
+    output_file = path_to_output + path_file.split("/")[-1].split(".")[0]
+    if del_exists(output_file+".exe"):
+        name = f"pyinstaller --onefile --distpath {path_to_output} {path_file}"
         subprocess.call(name, shell=True)
+        subprocess.call(f"mv {output_file} {output_file +'.exe'}", shell=True)
+        print(f"Pomyslnie wygenerowano plik exe do {path_file}")
     else:
-        print("Nie można wykonać operacji ze względu na błąd\nNajprawdopodobniej jest to brak uprawnień")
+        print(f"Nie można wykonac konwersji pliku {path_file}\nNajprawdopodobniej wina jest brak uprawnień")
 
 
 def del_exists(path_file):
